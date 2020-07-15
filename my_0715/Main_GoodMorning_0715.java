76be8a684cc55f5efd8c331673acf42ed93067c4
Sunaaaa <glglthssla@naver.com>
2020-07-15 23:16:14 +0900

    0715 좋은하루

---
+++ my_0715/Main_GoodMorning_0715.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_GoodMorning_0715 {
+    static int[] arr, result;
+    static int N, R, v;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R = sc.nextInt();
+
+        v = 0;
+        arr = new int[N];
+        result = new int[R];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // perm(0);
+        // permDup(0);
+        // comb(0,0);
+        // combDup(0,0);
+        subset(0);
+
+    }
+
+    static void perm(int idx){
+        if (idx == R) {
+            System.out.println(Arrays.toString(result));
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if ((v & (1<<i))==0) {
+                result[idx] = arr[i];
+                v |= (1<<i);
+                perm(idx+1);
+                v &= ~(1<<i);
+            }
+        }
+    }
+
+    static void permDup(int idx){
+        if (idx == R) {
+            System.out.println(Arrays.toString(result));
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            result[idx] = arr[i];
+            permDup(idx+1);
+        }
+    }
+
+    static void comb(int idx, int cdx){
+        if (cdx == R) {
+            System.out.println(Arrays.toString(result));
+            return;
+        }
+        if (idx == N) return;
+
+        result[cdx] = arr[idx];
+        comb(idx+1, cdx+1);
+        comb(idx+1, cdx);
+    }
+
+    static void combDup(int idx, int cdx){
+        if (cdx == R) {
+            System.out.println(Arrays.toString(result));
+            return;
+        }
+        if (idx == N) return;
+
+        result[cdx] = arr[idx];
+        combDup(idx, cdx+1);
+        combDup(idx+1, cdx);
+    }
+
+    static void subset(int idx){
+        if (idx == visited.length){
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) {
+                    System.out.print(arr[i]+" ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+
+        visited[idx] = true;
+        subset(idx+1);
+        visited[idx] = false;
+        subset(idx+1);
+
+    }
+
+}
