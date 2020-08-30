faab0233eb14b235f2c9da25b8f3c255433235ef
Sunaaaa <glglthssla@naver.com>
2020-08-30 23:08:07 +0900

    좋은하루 0830

--- my_0829/Main_1806.java
+++ my_0829/Main_1806.java
@@ -15,7 +15,7 @@ public class Main_1806 {
         M = Integer.parseInt(st.nextToken());
 
         ans = Integer.MAX_VALUE;
-        numbers = new int[N];
+        numbers = new int[N+1];
         
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
@@ -26,9 +26,20 @@ public class Main_1806 {
         int start = 0;
         int end = 0;
         int sum = 0;
+
+        while (start <= N && end <= N) {
+            if (sum >= M && ans > end-start) {
+                ans = end-start;
+            } 
+            if (sum < M) {
+                sum += numbers[end++];
+            } else {
+                sum -= numbers[start++];
+            }
+        }
         
         
-        System.out.println(ans);
+        System.out.println(ans==Integer.MAX_VALUE?0:ans);
 
 
     }
