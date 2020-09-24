3b215d5549a0eba6ac5ed3be1b251bd4720a517e
Sunaaaa <glglthssla@naver.com>
2020-09-24 23:08:40 +0900

    0924 좋은하루

---
+++ my_0831/Solution_7793.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_7793 {
+
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,1,-1};
+    static char[][] map;
+    static int N, M;
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("000");
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // N = Integer.parseInt(st.nextToken());
+        // M = Integer.parseInt(st.nextToken());
+
+        // map = new char[N][M];
+        // for (int i = 0; i < N; i++) {
+        //     st = new StringTokenizer(br.readLine());
+        //     for (int j = 0; j < M; j++) {
+        //         map[i][j] = st.nextToken().charAt(0);
+        //     }
+        // }
+
+        // for (int i = 0; i < N; i++) {
+        //     for (int j = 0; j < M; j++) {
+        //         System.out.print(map[i][j]+"");
+        //     }
+        //     System.out.println();
+        // }
+
+
+    }
+
+}
